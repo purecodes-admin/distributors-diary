8dce2ef63daa86e68fb30f06752c9cdc8f527e40
purecodes-admin <77955660+purecodes-admin@users.noreply.github.com>
2021-04-15 12:37:23 +0500

    Enable/disable submit button

--- resources/views/items/store.blade.php
+++ resources/views/items/store.blade.php
@@ -76,14 +76,14 @@
             </div>
 
             <div class="flex flex-col md:w-1/2 mt-2">
-                <button class="bg-blue-700 hover:bg-blue-900 font-bold text-white ml-2 py-2 rounded" type="button"
-                    onclick="return validateForm()">Add</button><br>
+                <button class="disabled:opacity-50 bg-blue-700 hover:bg-blue-900 font-bold text-white ml-2 py-2 rounded" type="button"
+                    onclick="return validateForm(this)">Add</button><br>
             </div>
         </form>
     </div>
 
     <script>
-        function validateForm() {
+        function validateForm(e) {
             window.setTimeout("document.getElementById('success').style.display='none';", 3000);
             window.setTimeout("document.getElementById('danger').style.display='none';", 3000);
 
@@ -115,7 +115,8 @@
                 document.getElementById("r_pricemsg1").style.display = ""
                 return false;
             }
-
+            
+            $(e).prop('disabled', true);
             $.ajax({
                 type: 'POST',
                 url: 'add',
@@ -131,6 +132,9 @@
                 },
                 error: function(res) {
                     document.getElementById("danger").style.display = ""
+                },
+                complete: function(res) {
+                    $(e).prop('disabled', false);
                 }
             });
 
